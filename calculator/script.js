fa5e443a573d982d2dd23d992f07d41a10be1a1d
abdikeem <abdihakimabdirahman21@gmail.com>
2022-10-02 02:44:41 +0300

    script.js

--- calculator/script.js
+++ calculator/script.js
@@ -2,15 +2,13 @@ function display(val)
 {
  document.getElementById("textvalue").value+=val
  }
-
 function evaluate()
 {
  let x = document.getElementById("textvalue").value
  let y = eval(x)
  document.getElementById("textvalue").value = y
 }
-
 function clear()
 {
-    document.getElementById("textvalue").value = ""
+ document.getElementById("textvalue").value = ""
 }
